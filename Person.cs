0869bbeba2505aac3e4efb44429af51b291b6bea
MiaoHatola <Jalon0@gmail.com>
2015-03-15 19:42:37 +0200

    Update Person.cs

--- Person.cs
+++ Person.cs
@@ -1,1 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
+namespace Logic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        private void login()
+        {
+
+        }
+    }
+}
